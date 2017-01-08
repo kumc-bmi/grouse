b7128b4a8cc9cb093b8d26f271cb257ccfd77f27
Nathan Graham <ngraham@kumc.edu>
2017-01-08 13:18:37 -0600

    Towards CMS deid, create bene_id mapping.
    Also added script to generate CMS deid SQL and the result (cms_deid.sql).

---
+++ deid/bene_id_mapping.sql
@@ -0,0 +1,63 @@
+-- bene_id_mapping.sql: Create bene_id mapping table for CMS deidentification
+-- Copyright (c) 2017 University of Kansas Medical Center
+
+whenever sqlerror continue;
+drop sequence bene_id_deid_seq;
+drop table bene_id_mapping;
+whenever sqlerror exit;
+
+create sequence bene_id_deid_seq
+  start with 1
+  increment by 1;
+
+create table bene_id_mapping (
+  BENE_ID VARCHAR2(15),
+  BENE_ID_DEID VARCHAR2(15),
+  DATE_SHIFT_DAYS NUMBER
+  );
+alter table bene_id_mapping parallel (degree 12);
+
+--select 
+--'select /*+ PARALLEL(' || table_name ||',12) */ distinct bene_id from ' || table_name || ' union'
+--from dba_tables where owner = 'CMS_ID_SAMPLE' and table_name not like 'REF_%' and table_name != 'BENE_ID_MAPPING';
+insert /*+ APPEND */ into bene_id_mapping
+select 
+  ubid.bene_id bene_id,
+  to_char(bene_id_deid_seq.nextval) bene_id_deid,
+  round(dbms_random.value(-364,0)) date_shift_days
+from (
+  select /*+ PARALLEL(HHA_OCCURRNCE_CODES,12) */ distinct bene_id from HHA_OCCURRNCE_CODES union
+  select /*+ PARALLEL(PDE,12) */ distinct bene_id from PDE union
+  select /*+ PARALLEL(BCARRIER_CLAIMS,12) */ distinct bene_id from BCARRIER_CLAIMS union
+  select /*+ PARALLEL(HHA_VALUE_CODES,12) */ distinct bene_id from HHA_VALUE_CODES union
+  select /*+ PARALLEL(HOSPICE_OCCURRNCE_CODES,12) */ distinct bene_id from HOSPICE_OCCURRNCE_CODES union
+  select /*+ PARALLEL(MBSF_D_CMPNTS,12) */ distinct bene_id from MBSF_D_CMPNTS union
+  select /*+ PARALLEL(HHA_CONDITION_CODES,12) */ distinct bene_id from HHA_CONDITION_CODES union
+  select /*+ PARALLEL(PDE_SAF,12) */ distinct bene_id from PDE_SAF union
+  select /*+ PARALLEL(OUTPATIENT_REVENUE_CENTER,12) */ distinct bene_id from OUTPATIENT_REVENUE_CENTER union
+  select /*+ PARALLEL(HHA_SPAN_CODES,12) */ distinct bene_id from HHA_SPAN_CODES union
+  select /*+ PARALLEL(HOSPICE_REVENUE_CENTER,12) */ distinct bene_id from HOSPICE_REVENUE_CENTER union
+  select /*+ PARALLEL(HHA_BASE_CLAIMS,12) */ distinct bene_id from HHA_BASE_CLAIMS union
+  select /*+ PARALLEL(MAXDATA_LT,12) */ distinct bene_id from MAXDATA_LT union
+  select /*+ PARALLEL(MAXDATA_OT,12) */ distinct bene_id from MAXDATA_OT union
+  select /*+ PARALLEL(BCARRIER_LINE,12) */ distinct bene_id from BCARRIER_LINE union
+  select /*+ PARALLEL(MAXDATA_RX,12) */ distinct bene_id from MAXDATA_RX union
+  select /*+ PARALLEL(MAXDATA_PS,12) */ distinct bene_id from MAXDATA_PS union
+  select /*+ PARALLEL(HOSPICE_SPAN_CODES,12) */ distinct bene_id from HOSPICE_SPAN_CODES union
+  select /*+ PARALLEL(HOSPICE_CONDITION_CODES,12) */ distinct bene_id from HOSPICE_CONDITION_CODES union
+  select /*+ PARALLEL(HHA_REVENUE_CENTER,12) */ distinct bene_id from HHA_REVENUE_CENTER union
+  select /*+ PARALLEL(MAXDATA_IP,12) */ distinct bene_id from MAXDATA_IP union
+  select /*+ PARALLEL(MEDPAR_ALL,12) */ distinct bene_id from MEDPAR_ALL union
+  select /*+ PARALLEL(HOSPICE_VALUE_CODES,12) */ distinct bene_id from HOSPICE_VALUE_CODES union
+  select /*+ PARALLEL(MBSF_AB_SUMMARY,12) */ distinct bene_id from MBSF_AB_SUMMARY union
+  select /*+ PARALLEL(OUTPATIENT_SPAN_CODES,12) */ distinct bene_id from OUTPATIENT_SPAN_CODES union
+  select /*+ PARALLEL(HOSPICE_BASE_CLAIMS,12) */ distinct bene_id from HOSPICE_BASE_CLAIMS union
+  select /*+ PARALLEL(OUTPATIENT_BASE_CLAIMS,12) */ distinct bene_id from OUTPATIENT_BASE_CLAIMS union
+  select /*+ PARALLEL(OUTPATIENT_OCCURRNCE_CODES,12) */ distinct bene_id from OUTPATIENT_OCCURRNCE_CODES union
+  select /*+ PARALLEL(OUTPATIENT_VALUE_CODES,12) */ distinct bene_id from OUTPATIENT_VALUE_CODES union
+  select /*+ PARALLEL(OUTPATIENT_CONDITION_CODES,12) */ distinct bene_id from OUTPATIENT_CONDITION_CODES
+  ) ubid;
+commit;
+
+create unique index bene_id_mapping_bid_idx on bene_id_mapping (bene_id);
+create unique index bene_id_mapping_deidbid_idx on bene_id_mapping (bene_id_deid);
---
+++ deid/gen_deid_sql.py
@@ -0,0 +1,83 @@
+''' Parse Oracle "create table" .sql file and generate sql to de-identify
+CMS data.
+'''
+from contextlib import contextmanager
+from re import findall, DOTALL
+
+
+def main(open_rd_argv, get_input_path):
+    with open_rd_argv(get_input_path()) as fin:
+        tables = tables_columns(fin.read())
+    for table, cols in tables.items():
+        sql = ('insert /*+ APPEND */ into "&&deid_schema".%(table)s\n'
+               'select /*+ PARALLEL(%(table)s,12) */ \n' %
+               dict(table=table, cols=',\n'.join(['  ' + c[0] for c in cols])))
+        for idx, (col, ctype) in enumerate(cols):
+            if ctype == 'DATE':
+                sql += ('  idt.%(col)s + bm.date_shift_days %(col)s'
+                        % dict(col=col))
+            elif col == 'BENE_ID':
+                sql += ('  bm.BENE_ID_DEID %(col)s'
+                        % dict(col=col))
+            elif (('ZIP' in col and 'PRVDR' not in col) or
+                  ('COUNTY' in col)):
+                sql += '  NULL %(col)s' % dict(col=col)
+            else:
+                sql += '  idt.%(col)s' % dict(col=col)
+
+            if idx != len(cols) - 1:
+                sql += ','
+            sql += '\n'
+
+        sql += ('from %(table)s idt \n'
+                'join bene_id_mapping bm '
+                'on bm.bene_id = idt.bene_id;\n\n')
+        print sql % dict(table=table)
+
+
+def tables_columns(sql):
+    '''
+    >>> sql = """
+    ... create table outpatient_value_codes (
+    ...  BENE_ID VARCHAR2(15),
+    ... CLM_VAL_AMT NUMBER
+    ... );
+    ...
+    ... create table outpatient_base_claims (
+    ...  BENE_ID VARCHAR2(15),
+    ... CLM_ID VARCHAR2(15)
+    ... );
+    ... """
+    >>> tables_columns(sql)
+    ... # doctest: +NORMALIZE_WHITESPACE
+    {'outpatient_value_codes': [['BENE_ID', 'VARCHAR2(15)'],
+                                ['CLM_VAL_AMT', 'NUMBER']],
+     'outpatient_base_claims': [['BENE_ID', 'VARCHAR2(15)'],
+                                ['CLM_ID', 'VARCHAR2(15)']]}
+    '''
+    td = dict()
+    for table, columns in findall('create table ([a-zA-Z_]+) \((.*?)\);',
+                                  sql, flags=DOTALL):
+        td[table] = [c.split(' ') for c in [ct.replace(',', '').strip()
+                                            for ct in
+                                            columns.split('\n')] if c]
+    return td
+
+
+if __name__ == '__main__':
+    def _tcb():
+        from sys import argv
+
+        def get_input_path():
+            return argv[1]
+
+        @contextmanager
+        def open_rd_argv(path):
+            if get_input_path() not in path:
+                raise RuntimeError('%s not in %s' % (get_input_path(), path))
+            with open(path, 'rb') as fin:
+                yield fin
+
+        main(open_rd_argv, get_input_path)
+
+    _tcb()
