9afc93aa9597e62fbc67b4eba6b47b3545ae5170
Dan Connolly <dckc@madmode.com>
2017-02-10 19:27:48 -0600

    document SQL script style, conventions

--- etl_i2b2/CONTRIBUTING.md
+++ etl_i2b2/CONTRIBUTING.md
@@ -1,5 +1,10 @@
-ISSUE: SQL style. See kumc-bmi-sql-style.xml sqldeveloper style
-indentation: 2 space, 120 chars max.
-<uppercase>3</uppercase> all lower case
+## SQL Style and Conventions
 
+SQL is written in lowercase, indented 2 spaces. More details are in
+the evolving kumc-bmi-sql-style.xml sqldeveloper style profile.
+
+TODO: 120 chars max.
 TODO: line break before ;
+
+Each script should start with a header comment and some
+dependency-checking queries. See `script_lib.py` for details.
--- etl_i2b2/script_lib.py
+++ etl_i2b2/script_lib.py
@@ -1,3 +1,60 @@
+r'''script_lib -- library of SQL scripts
+
+Scripts are pkg_resources, i.e. design-time constants.
+
+Each script should have a title, taken from the first line::
+
+    >>> Script.cms_patient_mapping.title()
+    'map CMS beneficiaries to i2b2 patients'
+
+    >>> fname, text = Script.cms_patient_mapping.value
+    >>> lines = text.split('\n')
+    >>> print lines[0]
+    /** cms_patient_mapping - map CMS beneficiaries to i2b2 patients
+
+TODO: copyright, license blurb enforcement
+
+We can separate the script into statements::
+
+    >>> statements = Script.cms_patient_mapping.statements()
+    >>> print next(s for s in statements if 'insert' in s)
+    ... #doctest: +ELLIPSIS
+    insert /*+ append */
+      into "&&I2B2STAR".patient_mapping
+    ...
+
+Dependencies between scripts are declared as follows::
+
+    >>> print next(decl for decl in statements if "'dep'" in decl)
+    select active from i2b2_status where 'dep' = 'i2b2_crc_design.sql'
+
+    >>> Script.cms_patient_mapping.deps()
+    ... #doctest: +ELLIPSIS
+    [<Script(i2b2_crc_design)>, <Script(cms_dem_txform)>, ...]
+
+We statically detect relevant effects; i.e. tables and views created::
+
+    >>> Script.cms_ccw_spec.created_objects()
+    [(<Script(cms_ccw_spec)>, ('view', 'cms_ccw'))]
+
+as well as tables inserted into::
+
+    >>> Script.cms_patient_mapping.inserted_tables(
+    ...     variables={'I2B2STAR': 'i2b2demodata'})
+    [(<Script(cms_patient_mapping)>, '"i2b2demodata".patient_mapping')]
+
+The last statement should be a scalar query that returns non-zero to
+signal that the script is complete:
+
+    >>> print statements[-1]
+    select count(*) complete
+    from "&&I2B2STAR".patient_mapping
+
+TODO: indexes.
+ISSUE: truncate, delete, update aren't reversible.
+
+'''
+
 import re
 
 import enum
@@ -10,31 +67,7 @@ from sql_syntax import (
 I2B2STAR = 'I2B2STAR'  # cf. &&I2B2STAR in sql_scripts
 
 
-class Script(enum.Enum):
-    [
-        cms_ccw_spec,
-        cms_dem_dstats,
-        cms_dem_load,
-        cms_dem_txform,
-        cms_patient_dimension,
-        cms_patient_mapping,
-        i2b2_crc_design
-    ] = [
-        (fname, pkg.resource_string(__name__, 'sql_scripts/' + fname))
-        for fname in [
-                'cms_ccw_spec.sql',
-                'cms_dem_dstats.sql',
-                'cms_dem_load.sql',
-                'cms_dem_txform.sql',
-                'cms_patient_dimension.sql',
-                'cms_patient_mapping.sql',
-                'i2b2_crc_design.sql'
-        ]
-    ]
-
-    def __repr__(self):
-        return '<%s(%s)>' % (self.__class__.__name__, self.name)
-
+class ScriptMixin(object):
     def statements(self,
                    variables=None):
         _name, text = self.value
@@ -43,12 +76,6 @@ class Script(enum.Enum):
             for _line, _comment, statement in iter_statement(text)]
 
     def created_objects(self):
-        '''
-        >>> Script.cms_ccw_spec.created_objects()
-        [(<Script(cms_ccw_spec)>, ('view', 'cms_ccw'))]
-
-        TODO: indexes
-        '''
         return [(dep, obj)
                 for dep in ([self] + self.deps())
                 for (_name, text) in [dep.value]
@@ -57,11 +84,6 @@ class Script(enum.Enum):
 
     def inserted_tables(self,
                         variables={}):
-        '''
-        >>> Script.cms_patient_mapping.inserted_tables(
-        ...     variables={'I2B2STAR': 'i2b2demodata'})
-        [(<Script(cms_patient_mapping)>, '"i2b2demodata".patient_mapping')]
-        '''
         return [(dep, obj)
                 for dep in ([self] + self.deps())
                 for (_name, text) in [dep.value]
@@ -85,11 +107,6 @@ class Script(enum.Enum):
     def _get_deps(cls, sql):
         '''
         >>> ds = Script._get_deps(
-        ...     "select col from t where 'dep' = 'cms_dem_txform.sql'")
-        >>> ds == [Script.cms_dem_txform]
-        True
-
-        >>> ds = Script._get_deps(
         ...     "select col from t where 'dep' = 'oops.sql'")
         Traceback (most recent call last):
             ...
@@ -107,3 +124,37 @@ class Script(enum.Enum):
         if not deps:
             raise KeyError(name)
         return deps
+
+
+class Script(ScriptMixin, enum.Enum):
+    '''Script is an enum.Enum of (fname, contents) tuples.
+
+    ISSUE: It's tempting to consider separate libraries for NAACCR,
+           NTDS, etc., but that doesn't integrate well with the
+           generic luigi.EnumParameter() in etl_tasks.SqlScriptTask.
+
+    '''
+    [
+        # Keep sorted
+        cms_ccw_spec,
+        cms_dem_dstats,
+        cms_dem_load,
+        cms_dem_txform,
+        cms_patient_dimension,
+        cms_patient_mapping,
+        i2b2_crc_design
+    ] = [
+        (fname, pkg.resource_string(__name__, 'sql_scripts/' + fname))
+        for fname in [
+                'cms_ccw_spec.sql',
+                'cms_dem_dstats.sql',
+                'cms_dem_load.sql',
+                'cms_dem_txform.sql',
+                'cms_patient_dimension.sql',
+                'cms_patient_mapping.sql',
+                'i2b2_crc_design.sql'
+        ]
+    ]
+
+    def __repr__(self):
+        return '<%s(%s)>' % (self.__class__.__name__, self.name)
