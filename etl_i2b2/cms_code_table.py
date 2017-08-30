a8778c74f2ceac3703eed80360b47220e19cede2
Dan Connolly <dconnolly@kumc.edu>
2017-08-30 14:42:07 -0500

    cms_code_table: clean up claim type experiment

--- etl_i2b2/cms_code_table.py
+++ etl_i2b2/cms_code_table.py
@@ -5,41 +5,11 @@ from hashlib import sha1
 from pathlib import Path  # use the type only; the constructor is ambient authority
 from sys import stderr
 from typing import Callable, List, Tuple, Type, TypeVar
+from urllib.parse import urljoin
 from urllib.request import OpenerDirector
-
 from xml.etree import ElementTree as ET
 
-url1 = 'https://www.resdac.org/cms-data/variables/medpar-nch-claim-type-code'
 Item = Tuple[str, str]
-
-
-def _claim_type(content: str) -> str:
-    return _markup(_items(content))
-
-
-def _items(content: str) -> List[Item]:
-    _skip, content = content.split('<table ', 1)
-    content, _skip = content.split('</table>', 1)
-    content = '<table ' + content + '</table>'
-    table = ET.fromstring(content)
-    trs = table.findall('.//tr')
-    rows = [[''.join(cell.itertext())
-             for cell in tr.findall('.//td')]
-            for tr in trs]
-    items = [(code, value)
-             for row in rows if row
-             for (code, value) in [row]]
-    return items
-
-
-def _markup(items: List[Item]) -> str:
-    table = ET.Element('table')
-    for code, value in items:
-        item = ET.SubElement(table, 'item',
-                             code=code, value=value)
-        item.tail = '\n'
-    raw = ET.tostring(table)  # type: bytes
-    return raw.decode('utf-8')
 
 
 C = TypeVar('C', bound='Cache')
@@ -81,9 +51,56 @@ class Cache(object):
             return self.download(addr, sha1sum)
 
 
-def _integration_test(build_opener: Callable[[], OpenerDirector]) -> None:
-    web = build_opener()
-    content = web.open(url1).read().decode('utf-8')
+class ResDACDoc(object):
+    '''pathlib style API to ResDAC docs
+    '''
+    base = 'https://www.resdac.org/cms-data/'
+
+    def __init__(self, web: OpenerDirector, path=''):
+        self.joinpath = lambda other: ResDACDoc(web, urljoin(path, other))
+        self.open = lambda: web.open(urljoin(self.base, path))
+
+    def __truediv__(self, other):
+        return self.joinpath(other)
+
+
+def _claim_type(content: str) -> str:
+    '''Render claim type table as XML
+
+    prototyping an idea for table constants in Oracle SQL
+    '''
+    return _markup(_items(content))
+
+
+def _items(content: str) -> List[Item]:
+    _skip, content = content.split('<table ', 1)
+    content, _skip = content.split('</table>', 1)
+    content = '<table ' + content + '</table>'
+    table = ET.fromstring(content)
+    trs = table.findall('.//tr')
+    rows = [[''.join(cell.itertext())
+             for cell in tr.findall('.//td')]
+            for tr in trs]
+    items = [(code, value)
+             for row in rows if row
+             for (code, value) in [row]]
+    return items
+
+
+def _markup(items: List[Item]) -> str:
+    table = ET.Element('table')
+    for code, value in items:
+        item = ET.SubElement(table, 'item',
+                             code=code, value=value)
+        item.tail = '\n'
+    raw = ET.tostring(table)  # type: bytes
+    return raw.decode('utf-8')
+
+
+def _integration_test(build_opener: Callable[[], OpenerDirector],
+                      clty='variables/medpar-nch-claim-type-code') -> None:
+    resdoc = ResDACDoc(build_opener())
+    content = (resdoc / clty).open().read().decode('utf-8')
     actual = "'" + _claim_type(content).replace("'", "''") + "'"
 
     [expected_start, expected_end] = '''
