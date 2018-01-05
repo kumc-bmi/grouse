2bd535935d656e06988675ff5d9d74836028276d
Dan Connolly <dconnolly@kumc.edu>
2018-01-04 23:14:02 -0600

    ProceduresLoad: exploring plans
    struggling to find a way to exploit key uniqueness
       - monotonically_increasing_id for proceduresid
       - pretty happy with partitioning by patient num
         - patient_groups() with ntile() might be overkill;
           we're only using the min and max overall
       - in px_meta() spark is winning:
         PushedFilters: [*EqualTo(M_APPLIED_PATH,@),  ...

--- etl_i2b2/cms_scalable_i2p.py
+++ etl_i2b2/cms_scalable_i2p.py
@@ -12,6 +12,7 @@ https://github.com/spotify/luigi/blob/master/examples/pyspark_wc.py
 
 from luigi.contrib.spark import PySparkTask
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as fun
 import luigi
 
 import param_val as pv
@@ -34,23 +35,15 @@ class JDBC4ETL(luigi.Task):
         from os import environ  # ISSUE: ambient
         return environ[self.passkey]
 
-    def table_access(self, spark_rw, name,
-                     partitionColumn=None, numPartitions=None):
-        properties = dict(user=self.user,
-                          password=self.__password,
-                          driver=self.driver,
-                          fetchsize=self.fetchsize,
-                          batchsize=self.batchsize)
-        if partitionColumn:
-            properties = dict(properties,
-                              lowerBound=0,  # ISSUE: compute these dynamically?
-                              upperBound=2000000,
-                              partitionColumn=partitionColumn,
-                              numPartitions=str(numPartitions))
-        return spark_rw.jdbc(
-            table=name,
-            url=self.db_url,
-            properties={k: str(v) for k, v in properties.items()})
+    def read(self, spark):
+        return (spark.read.format('jdbc')
+                .options(
+                    url=self.db_url,
+                    user=self.user,
+                    password=self.__password,
+                    driver=self.driver,
+                    fetchsize=self.fetchsize,
+                    batchsize=self.batchsize))
 
 
 class HelloCDM(PySparkTask):
@@ -71,8 +64,8 @@ class HelloCDM(PySparkTask):
 
     def main(self, sparkContext, *_args):
         spark = SparkSession(sparkContext)
-        harvest = self.db.table_access(
-            spark.read, '{t.schema}.harvest'.format(t=self))
+        harvest = self.db.read(spark).options(
+            dbtable='{t.schema}.harvest'.format(t=self)).load()
         harvest.write.save(self.output().path, format='csv')
 
 
@@ -82,6 +75,8 @@ class ProceduresLoad(PySparkTask):
     i2b2_star = pv.StrParam(default='grousedata')
     cdm = pv.StrParam(default='cms_pcornet_cdm')
     pat_group_qty = pv.IntParam(default=1000, significant=False)
+
+    proc_pat = r'(^(CPT|HCPCS|ICD10PCS):)|(^ICD9:\d{2}\.\d{1,2})'
 
     @property
     def db(self):
@@ -93,29 +88,72 @@ class ProceduresLoad(PySparkTask):
     def main(self, sparkContext, *_args):
         spark = SparkSession(sparkContext)
 
-        detail = {
-            'pcornet_proc': ('grousemetadata.pcornet_proc', None, None),
-            'observation_fact': ('{t.i2b2_star}.observation_fact'.format(t=self),
-                                 'patient_num', self.pat_group_qty),
-            'encounter': ('{t.cdm}.encounter'.format(t=self),
-                          'patid', self.pat_group_qty)
-        }
-        for alias, (jdbc_name, partitionColumn, numPartitions) in detail.items():
-            df = self.db.table_access(
-                spark.read, jdbc_name,
-                partitionColumn=partitionColumn, numPartitions=numPartitions)
-            df.createOrReplaceTempView(alias)
+        #@@ groups = self.patient_groups(spark).collect()
+        patParts = self.db.read(spark).options(
+            partitionColumn='patient_num',
+            numPartitions=self.pat_group_qty,
+            #@@lowerBound=groups[0].PATIENT_NUM_LO,
+            #@@upperBound=groups[-1].PATIENT_NUM_HI
+            lowerBound=0,
+            upperBound=25000000,
+        )
+        obs = patParts.options(
+            dbtable='''
+            (select * from {t.i2b2_star}.observation_fact
+            where regexp_like(concept_cd, '{t.proc_pat}')
+            and rownum < 1000  -- TODO@@@
+            )
+            '''.format(t=self)).load()
+        obs.printSchema()
+        obs.createOrReplaceTempView('observation_fact')
 
-        px_meta = spark.sql(PX_META_SQL.format(pcornet_proc='pcornet_proc'))
-        px_meta.createOrReplaceTempView('px_meta')
-        px_meta.cache()
+        enc = patParts.options(dbtable='{t.cdm}.encounter'.format(t=self)).load()
+        enc.createOrReplaceTempView('encounter')
+
+        self.px_meta(spark).createOrReplaceTempView('px_meta')
 
         proc = spark.sql(
             PROCEDURES_SQL.format(observation_fact='observation_fact',
                                   px_meta='px_meta',
                                   encounter='encounter'))
+
+        proc = proc.withColumn('proceduresid', fun.monotonically_increasing_id())
         proc.explain()
-        # proc.write.partionBy('patid').format('parquet').save(self.output().path)
+        # proc.show(20)
+        # proc.write.partitionBy('patid').format('parquet').save(self.output().path)
+
+    def px_meta(self, spark):
+        meta = self.db.read(spark).options(
+            dbtable='grousemetadata.pcornet_proc').load()
+        meta.createOrReplaceTempView('pcornet_proc')
+        px_meta = spark.sql(PX_META_SQL.format(pcornet_proc='pcornet_proc'))
+        #@@px_meta.createOrReplaceTempView('px_meta')
+        return px_meta.cache()
+
+    pat_grp_subq = '''
+    (
+        select group_num
+             , min(patient_num) patient_num_lo
+             , max(patient_num) patient_num_hi
+        from (
+          select patient_num
+               , ntile({group_qty}) over (order by patient_num) as group_num
+          from (
+            select /*+ parallel(20) */ distinct patient_num from {patient_dimension}
+            where patient_num is not null
+          ) ea
+        ) w_ntile
+        group by group_num
+        order by group_num
+    )
+    '''
+
+    def patient_groups(self, spark):
+        pd_name = '{t.i2b2_star}.patient_dimension'.format(t=self)
+        groups = self.db.read(spark).options(
+            dbtable=self.pat_grp_subq.format(patient_dimension=pd_name,
+                                             group_qty=self.pat_group_qty))
+        return groups.load().cache()
 
 
 # TODO: put SQL back in SQL files
@@ -137,8 +175,7 @@ where pr.c_fullname like '\PCORI\PROCEDURE\%'
 '''.replace('\\', '\\' * 4)  # ISSUE: C-style escapes
 
 PROCEDURES_SQL = r'''
-select monotonically_increasing_id() -- obs.upload_id || ' ' || obs.patient_num || ' ' || obs.instance_num PROCEDURESID ???
-     , obs.patient_num PATID
+select obs.patient_num PATID
      , obs.encounter_num ENCOUNTERID
      , nvl(enc.enc_type, 'NI') ENC_TYPE
      , enc.ADMIT_DATE
