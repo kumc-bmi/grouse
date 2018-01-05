b19ab04fb2b864e8be6b264f8a99bc245c3a4dad
Dan Connolly <dconnolly@kumc.edu>
2018-01-04 20:52:02 -0600

    ProceduresLoad: a spark SQL design
    The plan uses a SortMergeJoin on the observation_fact table, which I'm
    pretty sure we don't want.

--- etl_i2b2/cms_scalable_i2p.py
+++ etl_i2b2/cms_scalable_i2p.py
@@ -3,7 +3,10 @@
 TODO: migrate relevant module docs from cms_i2p
 
 
+
 clues from:
+https://cwiki.apache.org/confluence/display/Hive/LanguageManual
+
 https://github.com/spotify/luigi/blob/master/examples/pyspark_wc.py
 '''
 
@@ -23,20 +26,31 @@ class JDBC4ETL(luigi.Task):
     passkey = pv.StrParam(description='see client.cfg',
                           significant=False)
 
+    fetchsize = pv.IntParam(default=10000, significant=False)
+    batchsize = pv.IntParam(default=10000, significant=False)
+
     @property
     def __password(self):
         from os import environ  # ISSUE: ambient
         return environ[self.passkey]
 
-    def table_access(self, spark_rw, name):
+    def table_access(self, spark_rw, name,
+                     partitionColumn=None, numPartitions=None):
+        properties = dict(user=self.user,
+                          password=self.__password,
+                          driver=self.driver,
+                          fetchsize=self.fetchsize,
+                          batchsize=self.batchsize)
+        if partitionColumn:
+            properties = dict(properties,
+                              lowerBound=0,  # ISSUE: compute these dynamically?
+                              upperBound=2000000,
+                              partitionColumn=partitionColumn,
+                              numPartitions=str(numPartitions))
         return spark_rw.jdbc(
             table=name,
             url=self.db_url,
-            properties={
-                "user": self.user,
-                "password": self.__password,
-                "driver": self.driver,
-            })
+            properties={k: str(v) for k, v in properties.items()})
 
 
 class HelloCDM(PySparkTask):
@@ -53,7 +67,7 @@ class HelloCDM(PySparkTask):
         return JDBC4ETL()
 
     def output(self):
-        return luigi.LocalTarget(self.save_path)
+        return luigi.LocalTarget(self.save_path)  # ISSUE: ambient
 
     def main(self, sparkContext, *_args):
         spark = SparkSession(sparkContext)
@@ -64,25 +78,47 @@ class HelloCDM(PySparkTask):
 
 class ProceduresLoad(PySparkTask):
 
-    save_path = pv.StrParam(default='/tmp/px_meta.csv')
+    save_path = pv.StrParam(default='/tmp/procedures')
+    i2b2_star = pv.StrParam(default='grousedata')
+    cdm = pv.StrParam(default='cms_pcornet_cdm')
+    pat_group_qty = pv.IntParam(default=1000, significant=False)
 
     @property
     def db(self):
         return JDBC4ETL()
 
     def output(self):
-        return luigi.LocalTarget(self.save_path)
+        return luigi.LocalTarget(self.save_path)  # ISSUE: ambient
 
     def main(self, sparkContext, *_args):
         spark = SparkSession(sparkContext)
-        pcornet_proc = self.db.table_access(
-            spark.read, 'grousemetadata.pcornet_proc')  # TODO: input()
-        pcornet_proc.createOrReplaceTempView('pcornet_proc')
+
+        detail = {
+            'pcornet_proc': ('grousemetadata.pcornet_proc', None, None),
+            'observation_fact': ('{t.i2b2_star}.observation_fact'.format(t=self),
+                                 'patient_num', self.pat_group_qty),
+            'encounter': ('{t.cdm}.encounter'.format(t=self),
+                          'patid', self.pat_group_qty)
+        }
+        for alias, (jdbc_name, partitionColumn, numPartitions) in detail.items():
+            df = self.db.table_access(
+                spark.read, jdbc_name,
+                partitionColumn=partitionColumn, numPartitions=numPartitions)
+            df.createOrReplaceTempView(alias)
+
         px_meta = spark.sql(PX_META_SQL.format(pcornet_proc='pcornet_proc'))
-        px_meta.write.save(self.output().path, format='csv')
-        # TODO: the rest...
+        px_meta.createOrReplaceTempView('px_meta')
+        px_meta.cache()
+
+        proc = spark.sql(
+            PROCEDURES_SQL.format(observation_fact='observation_fact',
+                                  px_meta='px_meta',
+                                  encounter='encounter'))
+        proc.explain()
+        # proc.write.partionBy('patid').format('parquet').save(self.output().path)
 
 
+# TODO: put SQL back in SQL files
 PX_META_SQL = r'''
 select c_basecode concept_cd
      , SUBSTR(pr.pcori_basecode, INSTR(pr.pcori_basecode, ':') + 1, 11) px
@@ -100,3 +136,22 @@ where pr.c_fullname like '\PCORI\PROCEDURE\%'
   and pr.c_visualattributes like 'L%'
 '''.replace('\\', '\\' * 4)  # ISSUE: C-style escapes
 
+PROCEDURES_SQL = r'''
+select monotonically_increasing_id() -- obs.upload_id || ' ' || obs.patient_num || ' ' || obs.instance_num PROCEDURESID ???
+     , obs.patient_num PATID
+     , obs.encounter_num ENCOUNTERID
+     , nvl(enc.enc_type, 'NI') ENC_TYPE
+     , enc.ADMIT_DATE
+     , enc.PROVIDERID
+     , obs.start_date PX_DATE
+     , px_meta.PX
+     , px_meta.PX_TYPE
+     , 'CL' PX_SOURCE  -- (select Claim from px_source_enum)
+     , px_meta.c_name RAW_PX
+     , obs.upload_id RAW_PX_TYPE
+from {observation_fact} obs
+join {px_meta} px_meta on px_meta.concept_cd = obs.concept_cd
+-- ISSUE: prove that these left-joins match at most once.
+left join {encounter} enc on obs.patient_num = enc.patid
+                         and obs.encounter_num = enc.encounterid
+'''
