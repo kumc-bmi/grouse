98d953034d2d18d21619fa0a43bbc5102ea98bc6
Dan Connolly <dconnolly@kumc.edu>
2018-06-06 11:43:25 -0500

    sq_up_encdim_encounternum: avoid overlap with 2014

--- etl_i2b2/sql_scripts/mapping_reset.sql
+++ etl_i2b2/sql_scripts/mapping_reset.sql
@@ -11,7 +11,7 @@ drop sequence "&&I2B2STAR".sq_up_encdim_encounternum;
 whenever sqlerror exit;
 create sequence "&&I2B2STAR".sq_up_encdim_encounternum
   cache 1024
-  start with 1000000000;  -- avoid overlap with 2011-2013 medpar
+  start with 2000000000;  -- avoid overlap with 2011-2013, 2014 medpar
 
 truncate table "&&I2B2STAR".encounter_mapping;
 
