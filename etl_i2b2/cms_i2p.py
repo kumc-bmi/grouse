8d6f3723c9a003b70d8090b10a612da599b3c16b
Dan Connolly <dconnolly@kumc.edu>
2018-07-09 12:15:26 -0500

    prune dead ProceduresLoad class

--- etl_i2b2/cms_i2p.py
+++ etl_i2b2/cms_i2p.py
@@ -16,17 +16,14 @@ __ https://github.com/kumc-bmi/i2p-transform
 
 '''
 
-from typing import List, Tuple, cast
+from typing import List, cast
 
 import luigi
-import pandas as pd  # type: ignore
 
-from cms_pd import read_sql_step, obj_string
 from etl_tasks import DBAccessTask, I2B2Task, SqlScriptTask, LoggedConnection, log_plan
-from eventlog import EventLogger
 from param_val import IntParam, StrParam
 from script_lib import Script
-from sql_syntax import Environment, Params
+from sql_syntax import Environment
 
 
 class I2P(luigi.WrapperTask):
@@ -146,115 +143,3 @@ class FillTableFromView(_HarvestRefresh):
         log_plan(work, 'fill chunk of {table}'.format(table=self.table), {},
                  sql=step)
         work.execute(step)
-
-
-class ProceduresLoad(_HarvestRefresh):
-    table = StrParam(default='PROCEDURES')
-    pat_group_qty = IntParam(default=100, significant=False,
-                             description='bound enc_type mapping chunks too')
-    chunksize = IntParam(default=50000, significant=False)
-    parallel_degree = IntParam(default=12, significant=False)
-
-    obs_q = """
-        select /*+ parallel({degree}) */
-               upload_id, patient_num, instance_num
-             , encounter_num, start_date, concept_cd
-        from {i2b2_star}.observation_fact obs
-        where patient_num between :lo and :hi
-          and regexp_like(obs.concept_cd, :proc_pat)
-        """
-    proc_pat = r'(^(CPT|HCPCS|ICD10PCS):)|(^ICD9:\d{2}\.\d{1,2})'
-
-    def requires(self) -> List[luigi.Task]:
-        return [
-            self.project,  # I2B2 project
-            self.harvest,
-            SqlScriptTask(script=Script.cms_dx_dstats,
-                          param_vars=self.variables),
-        ]
-
-    def load(self, lc: LoggedConnection) -> None:
-        def merge(xwalk: pd.DataFrame, key: pd.Series) -> pd.DataFrame:
-            assert not any(xwalk.index.duplicated())
-            oob = ~key.isin(xwalk.index)
-            if any(oob):
-                lc.log.warning('%d %s (%0.1f%% of %d: %s...) out of bounds of %s',  # ISSUE
-                               len(key[oob]), key.name,
-                               100.0 * len(key[oob]) / len(key), len(key), key[:3].values,
-                               xwalk.columns.values)
-                key = key[~oob]
-            vals = xwalk.loc[key]
-            vals.index = key.index
-            return vals
-
-        px_meta = self.proc_code_map(lc)
-        pat_groups = self.project.patient_groups(lc, self.pat_group_qty)
-        target = '{ps}.{table}'.format(ps=self.harvest.schema, table=self.table)
-        proceduresid = 0
-
-        for _, _, lo, hi in pat_groups:
-            enc_type_map = self.enc_type_group(lc, (lo, hi))
-            for obs in pd.read_sql(self.obs_q
-                                   .format(i2b2_star=self.project.star_schema, degree=self.parallel_degree),
-                                   params=dict(proc_pat=self.proc_pat, lo=lo, hi=hi),
-                                   con=lc._conn,
-                                   chunksize=self.chunksize):
-                with lc.log.step('%(event)s into %(target)s %(obs_qty)d obs',
-                                 dict(event='insert observations', target=target, obs_qty=len(obs))) as step:
-                    px = merge(px_meta, key=obs.concept_cd)
-                    obs = obs[obs.concept_cd.isin(px_meta.index)]     # ISSUE: drop facts with no px_meta
-                    enc = merge(enc_type_map, key=obs.encounter_num)  # ISSUE: obs with no visit_dimension get null.
-                    proc = pd.DataFrame(dict(
-                        patid=obs.patient_num,
-                        encounterid=obs.encounter_num,
-                        enc_type=enc.enc_type,
-                        admit_date=enc.admit_date,
-                        providerid=enc.providerid,
-                        px_date=obs.start_date,
-                        px=px.px,
-                        px_type=px.px_type,
-                        px_source='CL',
-                        raw_px=obs.instance_num,
-                        raw_px_type=obs.upload_id),
-                    )
-                    proc.index = pd.RangeIndex(proceduresid, proceduresid + len(proc))
-                    proc.index.names = ['proceduresid']
-
-                    assert not any(proc.index.duplicated())
-
-                    proc.to_sql(schema=self.harvest.schema, name=self.table,
-                                con=lc._conn, dtype=obj_string(proc), if_exists='append')
-                    step.argobj.update(dict(proc_ix_start=proceduresid, proc_qty=len(proc)))
-                    step.msg_parts.append(' proceduresid %(proc_ix_start)d + %(proc_qty)d')
-                    proceduresid += len(proc)
-
-    @classmethod
-    def proc_code_map(cls, lc: LoggedConnection) -> pd.DataFrame:
-        xwalk = read_sql_step("""select * from px_meta""", lc).set_index('concept_cd')
-        lc.log.info('px_meta rows: %d', len(xwalk))
-        oops = xwalk[~xwalk.index.str.match(cls.proc_pat, as_indexer=True)]
-        if len(oops) > 0:
-            raise ValueError(oops, 'bug in px_meta view (from cms_dx_dstats.sql)')
-        return xwalk
-
-    def enc_type_group(self, lc: LoggedConnection, pat_range: Tuple[int, int]) -> pd.DataFrame:
-        lo, hi = pat_range
-        params = cast(Params, dict(lo=lo, hi=hi))
-        sql = """
-            select /*+ parallel({degree}) */ encounter_num
-                 , inout_cd enc_type, start_date admit_date, providerid
-            from {i2b2_star}.visit_dimension
-            where patient_num between :lo and :hi
-            """.format(i2b2_star=self.project.star_schema, degree=self.parallel_degree)
-        log_plan(lc, 'visit_dimension for pat group', params=params, sql=sql)
-        enc = read_sql_step(sql, lc,
-                            params=params, show_lines=2).set_index('encounter_num')
-        return _clean_dups(enc, lc.log)  # ISSUE: I found 3 IP encounters with duplicate encounter_num.
-
-
-def _clean_dups(df: pd.DataFrame, log: EventLogger) -> pd.DataFrame:
-    dup_ix = df.index.duplicated()
-    if any(dup_ix):
-        log.warning('%d dups; e.g.:\n%s', len(df[dup_ix]), df[dup_ix].head(40))
-        df = df[~dup_ix]
-    return df
