93a61ff545df6b18ab0bedf50e30df44ed00a7fc
Dan Connolly <dconnolly@kumc.edu>
2018-10-31 15:21:51 -0500

    migrate: punt partition exchange approach, for now

--- etl_i2b2/sql_scripts/migrate_fact_upload.sql
+++ etl_i2b2/sql_scripts/migrate_fact_upload.sql
@@ -1,30 +1,8 @@
 /** migrate_fact_upload -- append data from a workspace table.
-
-We use an observation_fact table partitioned by upload_id for ETL performance:
-
-create table &&I2B2STAR.observation_fact
- partition by hash(upload_id)
- ... ;
-
 */
 
-/* Post-hoc fix: observation_fact_NNNN chunks were created with
-   some missing null constraints and some different precision / lengths. */
-whenever sqlerror continue;
-drop table obs_fix_&&upload_id;
-whenever sqlerror exit;
-
-create table obs_fix_&&upload_id
-as select * from &&I2B2STAR.observation_fact where 1 = 0;
-
-insert /*+ append */ into obs_fix_&&upload_id
-select * from &&workspace_star.observation_fact_&&upload_id;
-
-
-alter table &&I2B2STAR.observation_fact
-exchange partition for (&&upload_id) with table obs_fix_&&upload_id;
-
-drop table obs_fix_&&upload_id;
+insert /*+ parallel(&&parallel_degree) append */ into &&I2B2STAR.observation_fact
+select * from &&workspace_star.observation_fact_&&upload_id ;
 
 insert into &&I2B2STAR.upload_status
 select * from &&workspace_star.upload_status where upload_id = &&upload_id
