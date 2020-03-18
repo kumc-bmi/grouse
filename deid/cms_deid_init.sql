0331f4dade7c1e58401e39cae645d63c1268ed5f
Xing Song <xsong@kumc.edu>
2020-03-18 16:11:01 -0500

    remove the tables for collecting dob_month_shift and min_max_dates

--- deid/cms_deid_init.sql
+++ deid/cms_deid_init.sql
@@ -1,5 +1,5 @@
 -- cms_deid_init.sql: Prepare for de-identification of CMS data
--- Copyright (c) 2017 University of Kansas Medical Center
+-- Copyright (c) 2020 University of Kansas Medical Center
 
 whenever sqlerror continue;
 drop table date_events;
@@ -17,22 +17,3 @@ create table date_events (
   DT DATE
   );
 alter table date_events parallel (degree 12);
-
--- min/max date events per either bene_id or msis_id + state_cd
-create table min_max_date_events (
-  BENE_ID VARCHAR2(15),
-  MSIS_ID VARCHAR2(32),
-  STATE_CD VARCHAR2(2),
-  MIN_DT DATE,
-  MAX_DT DATE
-  );
-alter table min_max_date_events parallel (degree 12);
-
--- Months to shift each person
-create table dob_shift (
-  BENE_ID VARCHAR2(15),
-  MSIS_ID VARCHAR2(32),
-  STATE_CD VARCHAR2(2),
-  DOB_SHIFT_MONTHS INTEGER
-  );
-alter table dob_shift parallel (degree 12);
